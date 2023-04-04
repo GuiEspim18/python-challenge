a66ce3e3793f94b89d3fded2c1ae1f4860500e03
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2023-04-04 18:01:25 -0300

    bug fix

--- challenge.py
+++ challenge.py
@@ -1,3 +1,5 @@
+import random
+
 # artes, parques, museus, pontos históricos, comércios, comida, hotéis e esportes
 error = "<ERRO> Digite um número que esteja na lista!"
 
@@ -21,13 +23,80 @@ Digite o número correspondente ao tipo de turismo:
 
 
 if escolha_usuario >= 1 and escolha_usuario <= 8:
-    tempo = int(input("Quanto tempo você tem disponível em horas?"))
+    # tempo = int(input("Quanto tempo você tem disponível em horas?"))
+    local = ""
 
-
+    if escolha_usuario == 1:
         
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "MASP"
+        elif aleatorio == 2:
+            local = "Pinacoteca"
+        elif aleatorio == 3:
+            local = "Theatro de São Paulo"
+
+    elif escolha_usuario == 2:
+        
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "Pátio do Colégio"
+        elif aleatorio == 2:
+            local = "Museu do Ipiranga"
+        elif aleatorio == 3:
+            local = "Mosteiro de São Bento"
+
+    elif escolha_usuario == 3:
+        
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "Rua 25 de Março"
+        elif aleatorio == 2:
+            local = "Galeria do Rock"
+        elif aleatorio == 3:
+            local = "Shopping Iguatemi"
+
+    elif escolha_usuario == 4:
+        
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "Rua 25 de Março"
+        elif aleatorio == 2:
+            local = "Galeria do Rock"
+        elif aleatorio == 3:
+            local = "Shopping Iguatemi"
+
+    elif escolha_usuario == 5:
+        
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "Fábrica da Bauducco"
+        elif aleatorio == 2:
+            local = "Feira da Liberdade"
+        elif aleatorio == 3:
+            local = "Restaurante Figueira Rubaiyat"
+
+    elif escolha_usuario == 6:
+        
+        aleatorio = random.randint(1, 3)
+
+        if aleatorio == 1:
+            local = "Estádio do Morumbi"
+        elif aleatorio == 2:
+            local = "Allianz Parque"
+        elif aleatorio == 3:
+            local = "Museu do Futebol"
+            
+    
+    print(f"De acordo com as suas imporfações o ponto turístico que recomendamos é {local}")
    
-    if tempo >= 1 and tempo <= 10:
-        print(f"Tendo em vista os dados de {name}, em {tempo} horas, relacionado ao tema {locais}")
+    # if tempo >= 1 and tempo <= 10:
+    #     print(f"Tendo em vista os dados de {name}, em {tempo} horas, relacionado ao tema {escolha_usuario}")
 
         
 
