2c13960740a8948d0ef8f7638d8c029a5dd9e4f2
lucassperanzini <RM98297@fiap.com.br>
2023-04-05 15:38:41 -0300

    fiz alterações

--- challenge.py
+++ challenge.py
@@ -12,77 +12,114 @@ Qual tipo de turismo você gostaria de fazer?
 
 1- Cultural ->  museus, galerias de arte, teatros e outros locais para conhecer a história e a cultura da cidade.
 2- Histórico -> monumentos, prédios antigos, sítios arqueológicos e outros locais para aprender sobre a história da cidade.
-3- Natureza ->  parques, reservas naturais, trilhas ecológicas e outros locais para apreciar a natureza e os ecossistemas da cidade.
-4- Comércios -> shoppings, feiras, lojas de artesanato e outros locais para comprar produtos típicos da cidade.
-5- Gastronomia -> restaurantes, mercados e feiras para experimentar a culinária local.
-6- Esportes ->  arenas esportivas, estádios e outros locais para assistir ou praticar esportes na cidade.
+3- Ambiental ->  parques, reservas naturais, trilhas ecológicas e outros locais para apreciar a natureza e os ecossistemas da cidade.
+4- Comercial -> shoppings, feiras, lojas de artesanato e outros locais para comprar produtos típicos da cidade.
+5- Gastronômico -> restaurantes, mercados e feiras para experimentar a culinária local.
+6- Esportivo ->  arenas esportivas, estádios e outros locais para assistir ou praticar esportes na cidade.
 
 Digite o número correspondente ao tipo de turismo:
 """))
 
-if escolha_usuario >= 1 and escolha_usuario <= 8:
-    # tempo = int(input("Quanto tempo você tem disponível em horas?"))
+if escolha_usuario >= 1 and escolha_usuario <= 6:
+    # tempo = int(input("Quanto tempo você tem disponível em horas?")
     local = ""
+    recomendacao1 = random.randint(1, 3)
+    recomendacao2 = random.randint(4, 6)
 
     match escolha_usuario: 
         case 1:
-            aleatorio = random.randint(1, 3)
+            tema = "Cultural"
 
-            if aleatorio == 1:
-                local = "MASP"
-            elif aleatorio == 2:
-                local = "Pinacoteca"
-            elif aleatorio == 3:
+            if recomendacao1 == 1 and recomendacao2 == 4:
+                local1 = "MASP"
+                local2 = "Pinacoteca"
+            elif recomendacao1 == 1 and recomendacao2 == 5:
                 local = "Theatro de São Paulo"
+                local2 = "MASP"
+            elif recomendacao1 == 1 and recomendacao2 == 6:
+                local = "Theatro de São Paulo"
+                local2 = "Pinacoteca"
+            
         case 2:
-            aleatorio = random.randint(1, 3)
+            tema = "Histórico"
 
-            if aleatorio == 1:
-                local = "Pátio do Colégio"
-            elif aleatorio == 2:
-                local = "Museu do Ipiranga"
-            elif aleatorio == 3:
+            if recomendacao1 == 1 and recomendacao2 == 4:
+                local1 = "Pátio do Colégio"
+                local2 = "Museu do Ipiranga"
+            elif recomendacao1 == 1 and recomendacao2 == 5:
                 local = "Mosteiro de São Bento"
-        case 3:
-            aleatorio = random.randint(1, 3)
+                local2 = "Museu do Ipiranga"
+            elif recomendacao1 == 1 and recomendacao2 == 6:
+                local = "Theatro de São Paulo"
+                local2 = "Pinacoteca"
 
-            if aleatorio == 1:
+        
+        case 3:
+            tema = "Ambiental"
+            recomendacao = random.randint(1, 3)
+
+            if recomendacao == 1:
                 local = "Parque do Ibirapuera"
-            elif aleatorio == 2:
+            elif recomendacao == 2:
                 local = "Jardim Botânico de São Paulo"
-            elif aleatorio == 3:
+            else: 
                 local = "Horto Florestal"
         case 4:
-            aleatorio = random.randint(1, 3)
+            tema = "Comércio"
+            recomendacao = random.randint(1, 3)
 
-            if aleatorio == 1:
+            if recomendacao == 1:
                 local = "Rua 25 de Março"
-            elif aleatorio == 2:
+            elif recomendacao == 2:
                 local = "Galeria do Rock"
-            elif aleatorio == 3:
+            else:
                 local = "Shopping Iguatemi"    
         case 5:
-            aleatorio = random.randint(1, 3)
+            tema = "Gastronômico"
+            recomendacao = random.randint(1, 3)
 
-            if aleatorio == 1:
+            if recomendacao == 1:
                 local = "Fábrica da Bauducco"
-            elif aleatorio == 2:
+            elif recomendacao == 2:
                 local = "Feira da Liberdade"
-            elif aleatorio == 3:
+            else:
                 local = "Restaurante Figueira Rubaiyat"
         case 6:
-            aleatorio = random.randint(1, 3)
+            tema = "Esportivo"
 
-            if aleatorio == 1:
+            recomendacao = random.randint(1, 3)
+
+            if recomendacao == 1:
                 local = "Estádio do Morumbi"
-            elif aleatorio == 2:
+            elif recomendacao == 2:
                 local = "Allianz Parque"
-            elif aleatorio == 3:
+            else:
                 local = "Museu do Futebol"
         case _:
             print(error)
+
+    transporte = int(input("""Qual meio de transporte você quer escolher? 
+
+    1 - Uber
+    2 - Metrô
+    3 - Ônibus
+    4 - A pé
+    """))
+
+    match transporte: 
+        case 1:
+            transporte = "Uber"
+        case 2:
+            transporte = "Metrô"
+        case 3:
+            transporte = "Ônibus"
+        case 4:
+            transporte = "A pé"
+
+
+
     
-    print(f"De acordo com as suas imporfações o ponto turístico que recomendamos é {local}")
+    print(f"De acordo com as suas informações, o tipo de turismo escolhido é {tema}, o seu transporte será {transporte} e o trajeto que recomendamos é {local}")
     
 else:
     print(error)
