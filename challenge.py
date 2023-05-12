c1e69f908a2ce425e352e5c442664e80f7b2a94d
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-05-12 18:56:47 -0300

    Verificação e loop da escolha de turismo

--- challenge.py
+++ challenge.py
@@ -1,44 +1,48 @@
+# COLOCA O TRY-EXCEPT AE MANO
+
 import random
 import re
 error = ""
-continuar = True
+continuar = continuar2 = True
+escolha_usuario = 0
+
 
 while continuar == True:
-    try:
-        print("Bem vindo ao PathFinder!\nDigite o seu nome: ")
-        name = input()
+    print("Bem vindo ao PathFinder!\nDigite o seu nome: ")
+    name = input()
 
-        if re.search("\d", name):
-            error = "<ERRO>O nome não pode conter dígitos"
-            raise ValueError
-            continuar = False
+    if re.search("\d", name):
+        print("ERRO: O nome não pode conter números.")
+    else:
+        break
 
-        else:
-            print(f"Olá, {name}!")
+while continuar2 == True:
+# while not (escolha_usuario >=1) and (escolha_usuario <=6):
+    # try: 
+    print(f"Olá, {name}!")
+    # while not (escolha_usuario >=1) and (escolha_usuario <=6):
+    escolha_usuario = int(input("""
+Qual tipo de turismo você gostaria de fazer?
 
-        escolha_usuario = int(input("""
-        Qual tipo de turismo você gostaria de fazer?
+1- Cultural ->  Museus, galerias de arte, teatros e outros locais para conhecer a história e a cultura da cidade.
+2- Histórico -> Monumentos, prédios antigos, sítios arqueológicos e outros locais para aprender sobre a história da cidade.
+3- Ambiental ->  Parques, reservas naturais, trilhas ecológicas e outros locais para apreciar a natureza e os ecossistemas da cidade.
+4- Comercial -> Shoppings, feiras, lojas de artesanato e outros locais para comprar produtos típicos da cidade.
+5- Gastronômico -> Restaurantes, mercados e feiras para experimentar a culinária local.
+6- Esportivo ->  Arenas esportivas, estádios e outros locais para assistir ou praticar esportes na cidade.
 
-        1- Cultural ->  Museus, galerias de arte, teatros e outros locais para conhecer a história e a cultura da cidade.
-        2- Histórico -> Monumentos, prédios antigos, sítios arqueológicos e outros locais para aprender sobre a história da cidade.
-        3- Ambiental ->  Parques, reservas naturais, trilhas ecológicas e outros locais para apreciar a natureza e os ecossistemas da cidade.
-        4- Comercial -> Shoppings, feiras, lojas de artesanato e outros locais para comprar produtos típicos da cidade.
-        5- Gastronômico -> Restaurantes, mercados e feiras para experimentar a culinária local.
-        6- Esportivo ->  Arenas esportivas, estádios e outros locais para assistir ou praticar esportes na cidade.
+Digite o número correspondente ao tipo de turismo:
+"""))
 
-        Digite o número correspondente ao tipo de turismo:
-        """))
+    if escolha_usuario < 1 or escolha_usuario > 6:
+        print("ERRO: Escolha um número que esta na lista.")
 
-        if escolha_usuario < 1 or escolha_usuario > 6:
-            error = "<ERRO>Escolha um número que esta na lista"
-            raise ValueError
-            continuar = False
-
-        else:
-            local1 = ""
-            local2 = ""
-            recomendacao1 = random.randint(1, 3)
-            recomendacao2 = random.randint(4, 6)
+    else:
+        continuar2 == False
+        local1 = ""
+        local2 = ""
+        recomendacao1 = random.randint(1, 3)
+        recomendacao2 = random.randint(4, 6)
 
         match escolha_usuario:
             case 1:
@@ -65,5 +69,71 @@ while continuar == True:
                 elif (recomendacao1 == 1 or 2 or 3) and recomendacao2 == 6:
                     local1 = "Pátio do Colégio"
                     local2 = "Mosteiro de São Bento"
-    except ValueError:
-        print(error)
+            case 3:
+                tema = "Ambiental"
+
+                if (recomendacao1 == 1 or 2 or 3) and recomendacao2 == 4:
+                    local1 = "Parque do Ibirapuera"
+                    local2 = "Jardim Botânico de São Paulo"
+                elif (recomendacao1 == 1 or 2 or 3) and recomendacao2 == 5:
+                    local1 = "Horto Florestal"
+                    local2 = "Jardim Botânico de São Paulo"
+                elif (recomendacao1 == 1 or 2 or 3) and recomendacao2 == 6:
+                    local1 = "Parque do Ibirapuera"
+                    local2 = "Horto Florestal"
+            case 4:
+                tema = "Comércial"
+
+                if recomendacao1 == 1 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Rua 25 de Março"
+                    local2 = "Galeria do Rock"
+                elif recomendacao1 == 2 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Shopping Iguatemi"
+                    local2 = "Rua 25 de Março"
+                elif recomendacao1 == 3 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Galeria do Rock"
+                    local2 = "Shopping Iguatemi"
+            case 5:
+                tema = "Gastronômico"
+
+                if recomendacao1 == 1 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Fábrica da Bauducco"
+                    local2 = "Feira da Liberdade"
+                elif recomendacao1 == 2 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Restaurante Figueira Rubaiyat"
+                    local2 = "Fábrica da Bauducco"
+                elif recomendacao1 == 3 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Feira da Liberdade"
+                    local2 = "Restaurante Figueira Rubaiyat"
+            case 6:
+                tema = "Esportivo"
+
+                if recomendacao1 == 1 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Estádio do Morumbi"
+                    local2 = "Allianz Parque"
+                elif recomendacao1 == 2 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Museu do Futebol"
+                    local2 = "Estádio do Morumbi"
+                elif recomendacao1 == 3 and (recomendacao2 == 4 or 5 or 6):
+                    local1 = "Allianz Parque"
+                    local2 = "Museu do Futebol"
+
+        transporte = int(input("""Qual meio de transporte você quer escolher? 
+
+1 - Uber
+2 - Metrô
+3 - Ônibus
+4 - A pé
+
+Digite o número correspondente ao meio de transporte:
+                """))
+
+        match transporte:
+                case 1:
+                    transporte = "Uber"
+                case 2:
+                    transporte = "Metrô"
+                case 3:
+                    transporte = "Ônibus"
+                case 4:
+                    transporte = "A pé"
